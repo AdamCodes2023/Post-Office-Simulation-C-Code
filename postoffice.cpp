0f644f8dde759b110f42e13cc8d5d3d6d0aa9d64
AdamCodes2023 <122576141+AdamCodes2023@users.noreply.github.com>
2024-07-07 12:23:27 -0400

    Establish Vectors for Wait Times and Customer Queues and Begin Outlining the Normal Post Office Sequence of Events

--- postoffice.cpp
+++ postoffice.cpp
@@ -56,3 +56,75 @@ void PostOffice::removeFromVector(std::vector<int> &v, int pos) {
 int PostOffice::getVectorSize(std::vector<int> &v) {
   return v.size();
 } //End int PostOffice::getVectorSize(std::vector<int> &v)
+
+//Set the values of the private integers that the post office uses.
+void PostOffice::setPrivateIntValue(int &p, int n) {
+  p = n;
+} //End void PostOffice::setPrivateIntValue(int &p, int n)
+
+//Get the values of the private integers that the post office uses.
+int PostOffice::getPrivateIntValue(int &p) {
+  return p;
+} //End int PostOffice::getPrivateIntValue(int &p)
+
+//Decrement the values of the private integers that the post office uses.
+void PostOffice::decrementPrivateIntValue(int &p, int d) {
+  p -= d;
+} //End void PostOffice::decrementPrivateIntValue(int &p, int d)
+
+//Get the values from the private PostOffice vectors containing wait time and queue information.
+int PostOffice::getVectorElement(std::vector<int> &v, int e) {
+  return v[e];
+} //End int PostOffice::getVectorElement(std::vector<int> &v, int e)
+
+//Simulates a day at the post office (480 minutes).
+void PostOffice::normalDayLoop() {
+  cout << "POST OFFICE IS OPEN!" << endl;
+  bool employeeBusy = false;
+  int totalCustomers = 0;
+  int totalTaskTime = 0;
+  int totalTasks = 0;
+  int customersPastWorkDay = 0;
+  int forceCustomerArrival = 0;
+  setPrivateIntValue(newPersonArrivalTime, 0);
+  setPrivateIntValue(employeeTaskTime, 0);
+  for (int runTime = getPrivateIntValue(time); runTime > 0; runTime--) {
+    //POST THE TIME LEFT IN THE WORK DAY!
+    cout << endl << "TIME LEFT: " << runTime << endl << endl;
+    //A CUSTOMER HAS TO COME INTO THE POST OFFICE IF 5 MINUTES PASSES WITHOUT A NEW CUSTOMER ARRIVING.
+    forceCustomerArrival++;
+    //GENERATE A POST OFFICE CUSTOMER'S ARRIVAL TIME (1 - 5 MINUTES)!
+    setPrivateIntValue(newPersonArrivalTime, generatePerson());
+    //A POST OFFICE CUSTOMER ARRIVES (50% CHANCE)!
+    if (getPrivateIntValue(newPersonArrivalTime) == 1 || forceCustomerArrival == 5) {
+      addToVector(queue, 1); 
+      addToVector(arrivalTimes, runTime);
+      cout << "A PERSON ARRIVES!" << endl;
+      forceCustomerArrival = 0;
+    } //End if (newPersonArrivalTime == 0)
+    //GENERATE THE POSTAL EMPLOYEE'S TASK FOR THE NEXT CUSTOMER!
+    //REMOVE THE CUSTOMER FROM THE QUEUE ONCE THE POSTAL EMPLOYEE COMPLETES THE TASK!
+    if (employeeBusy == false) {
+      //EMPTY QUEUE CAUSES A MEMORY LEAK BECAUSE THERE IS NOBODY TO REMOVE FROM THE QUEUE!
+      if (getVectorSize(queue) > 0) {
+        setPrivateIntValue(employeeTaskTime, employeeCycle());
+        totalTaskTime += getPrivateIntValue(employeeTaskTime);
+        totalTasks++;
+        employeeBusy = true;
+        removeFromVector(queue, 1);
+        addToVector(departureTimes, runTime);
+        totalCustomers++;
+        cout << "POSTAL EMPLOYEE IS BUSY!" << endl;
+        cout << "CUSTOMER #" << totalCustomers << " WAITED " << getVectorElement(arrivalTimes, totalCustomers - 1) - getVectorElement(departureTimes, totalCustomers - 1) << " MINUTE(S)!" << endl;
+      } //End if (getVectorSize(queue) > 0)
+    } //End if (employeeBusy == false)
+    //POSTAL EMPLOYEE WORKS ON THE TASK!
+    decrementPrivateIntValue(employeeTaskTime, 1);
+    //FREE THE POSTAL EMPLOYEE FROM THE CURRENT TASK!
+    if (getPrivateIntValue(employeeTaskTime) == 0) {
+      employeeBusy = false;
+      cout << "A PERSON DEPARTS! THE POSTAL EMPLOYEE IS FREE!" << endl;
+    } //End if (employeeTaskTime == 0)
+    //ONE MINUTE PASSES!
+  } //End for (int runTime = time; runTime > 0; runTime--)
+}
